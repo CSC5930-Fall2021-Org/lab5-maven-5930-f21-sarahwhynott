30a28a32d9d03a7477067ba4130c5dc1a3606e30
sarahwhynott <swhynott@villanova.edu>
2021-11-28 17:15:44 -0500

    Comment added for Jenkins

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -4,6 +4,8 @@ public class ConsoleApp {
 
     public static void main(String[] args) {
         
+        //Comment added for Jenkins
+        
         System.out.println("Hello Maven");
         
     }
